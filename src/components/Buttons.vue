4769c21979c21bf216088b5b08ddfeabe09a240b
stalomeow <stalowork@163.com>
2024-04-06 21:43:35 +0800

    Initial

---
+++ src/components/Buttons.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { IconDefinition } from '@fortawesome/fontawesome-common-types'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+defineProps<{
+  links: {
+    url: string
+    title: string
+    icon: IconDefinition
+  }[]
+}>()
+
+</script>
+
+<template>
+  <div class="buttons">
+    <a
+      v-for="link in links"
+      :key="link.url"
+      :href="link.url"
+      :title="link.title"
+      class="button"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FontAwesomeIcon class="icon" :icon="link.icon" />
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.buttons {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.button {
+  display: block;
+  color: rgba(255, 255, 255, 0.87);
+  line-height: 1;
+  margin: 1rem;
+  transition: transform 0.2s;
+}
+
+.button:hover {
+  transform: scale(1.25);
+}
+
+.button .icon {
+  width: 1.5rem;
+  height: 1.5rem;
+  vertical-align: middle;
+}
+</style>
---
+++ src/pages/index/App.vue
@@ -0,0 +1,119 @@
+<script setup lang="ts">
+import { faEnvelope, faBlog, faMugSaucer } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faBilibili, faTelegram } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Buttons from "../../components/Buttons.vue";
+
+const links = [
+  {
+    url: 'https://github.com/stalomeow',
+    title: 'GitHub',
+    icon: faGithub,
+  },
+  {
+    url: 'https://space.bilibili.com/456366858',
+    title: 'Bilibili',
+    icon: faBilibili,
+  },
+  {
+    url: 'mailto:stalowork@163.com',
+    title: 'E-Mail',
+    icon: faEnvelope
+  },
+  {
+    url: 'https://t.me/StaloMeow',
+    title: 'Telegram',
+    icon: faTelegram,
+  },
+  {
+    url: 'https://note.stalomeow.com/blog',
+    title: 'Blog',
+    icon: faBlog,
+  },
+]
+</script>
+
+<template>
+  <img src="https://avatars.githubusercontent.com/u/47203031?v=4" class="logo" alt="Vite logo" />
+
+  <div class="info">
+    <span class="name">Stalo</span>
+    <span class="user-id">@stalomeow</span>
+  </div>
+
+  <Buttons :links="links" />
+
+  <a class="sponsor-button" href="/sponsor">
+    <FontAwesomeIcon class="icon" :icon="faMugSaucer" />
+    <p>Buy Me a Coffee</p>
+  </a>
+</template>
+
+<style scoped>
+.logo {
+  border-radius: 50%;
+  outline: 25rem solid #1f1f1f;
+  height: 12rem;
+  vertical-align: middle;
+}
+
+.info {
+  margin-top: 1.5rem;
+  margin-bottom: 0.5rem;
+  line-height: 1.5;
+}
+
+.name {
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding: 0.2rem;
+}
+
+.user-id {
+  color: #888;
+  font-size: 1.25rem;
+  padding: 0.2rem;
+}
+
+.sponsor-button {
+  display: block;
+  color: rgba(255, 255, 255, 0.87);
+  line-height: 1;
+  margin-top: 5rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+}
+
+.sponsor-button p {
+  margin-top: 0.5rem;
+  font-size: 0.8rem;
+  font-weight: bold;
+  transition: transform 0.2s;
+}
+
+.sponsor-button .icon {
+  width: 3rem;
+  height: 3rem;
+  vertical-align: middle;
+}
+
+.sponsor-button:hover .icon {
+  animation: tilt-shaking 0.2s 2;
+}
+
+.sponsor-button:hover p {
+  transform: scale(1.1);
+}
+
+@keyframes tilt-shaking {
+  0% { transform: rotate(0deg); }
+  25% { transform: rotate(5deg); }
+  50% { transform: rotate(0eg); }
+  75% { transform: rotate(-5deg); }
+  100% { transform: rotate(0deg); }
+}
+</style>
